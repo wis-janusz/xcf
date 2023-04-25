6ce402e37556f570307738506cfc76709256c4df
wis-janusz <wis.janusz@gmail.com>
2023-04-25 08:54:26 +0200

    added preliminary wordpiece tokenization

--- xcf/models/tokenizers.py
+++ xcf/models/tokenizers.py
@@ -1,16 +1,12 @@
-
 import pandas as pd
-AA_VOCAB = ['<PAD>','<START>','<END>','<MASK>','A','B','C','D','E','F','G','H','I','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-MAX_SEQUENCE_LENGTH = 600 #Not counting <START> and <END> tokens
 
-def tokenize_protein(seq:str):
+def tokenize_protein(seq:str, vocab, max_seq_len):
     tokens = [char for char in seq]
-    tokens += [AA_VOCAB[0]] * (MAX_SEQUENCE_LENGTH - len(tokens))
-    tokens.insert(0,AA_VOCAB[1])
-    tokens.append(AA_VOCAB[2])
+    tokens += [vocab[0]] * (max_seq_len - len(tokens))
+    tokens.insert(0,vocab[1])
+    tokens.append(vocab[2])
     ids = []
     for token in tokens:
-        ids.append(AA_VOCAB.index(token))
+        ids.append(vocab.index(token))
     return pd.Series([tokens, ids])
 
-print(len(AA_VOCAB))
